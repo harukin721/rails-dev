b18a53427448e15bdb01ccdc57d88289a62b0a5d
uejima-haruki <h.ueji.43@gmail.com>
2021-04-06 23:50:42 +0900

    掲示板のタグ機能を実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,12 @@ if Rails.env == 'development'
     (1..50).each do |i|
         Board.create(name: "ユーザ#{i}", title: "タイトル#{i}", body: "本文#{i}")
     end
+
+    Tag.create([
+        { name: 'Ruby1' },
+        { name: 'Ruby2' },
+        { name: 'Ruby3' },
+        { name: 'Ruby4' },
+        { name: 'Ruby5' },
+    ])
 end
